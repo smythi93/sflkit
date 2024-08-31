d7eebf85b74b00d19ca70a7ebc27982356d03624
Marius Smytzek <marius.smytzek@cispa.de>
2024-08-31 13:32:30 +0200

    added meta model to slice analyzer

--- src/sflkit/fendr/analyzer.py
+++ src/sflkit/fendr/analyzer.py
@@ -1,17 +1,22 @@
+import json
+import os
 from typing import Type, Optional, List, Callable, Set
 
 from sflkit import Analyzer
 from sflkit.analysis.analysis_type import AnalysisType, AnalysisObject
+from sflkit.analysis.analyzer import load_analysis_json, deserialize
 from sflkit.analysis.factory import AnalysisFactory
 from sflkit.analysis.suggestion import Suggestion
 from sflkit.model import EventFile
 from sflkit.fendr.models import TestSliceModel
+from sflkit.model.model import MetaModel
 
 
 class SliceAnalyzer(Analyzer):
     def __init__(
         self,
         model_class: Type[TestSliceModel],
+        meta_model: MetaModel = None,
         relevant_event_files: Optional[List[EventFile]] = None,
         irrelevant_event_files: Optional[List[EventFile]] = None,
         factory: Optional[AnalysisFactory] = None,
@@ -20,6 +25,7 @@ class SliceAnalyzer(Analyzer):
             relevant_event_files=relevant_event_files,
             irrelevant_event_files=irrelevant_event_files,
             factory=factory,
+            meta_model=meta_model,
             model_class=model_class,
         )
 
@@ -41,4 +47,16 @@ class SliceAnalyzer(Analyzer):
     ) -> List[Suggestion]:
         return super().get_sorted_suggestions_from_analysis(
             base_dir, analysis, metric, use_weight=True
+        )
+
+    @staticmethod
+    def load_with_slice(path: os.PathLike, model_class: Type[TestSliceModel]):
+        return SliceAnalyzer(
+            model_class, meta_model=MetaModel(load_analysis_json(path))
+        )
+
+    @staticmethod
+    def loads_with_slice(data: str, model_class: Type[TestSliceModel]):
+        return SliceAnalyzer(
+            model_class, meta_model=MetaModel(set(map(deserialize, json.loads(data))))
         )
